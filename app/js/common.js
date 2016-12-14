258bb21b642297e718523f0f736f56a17aa7acec
lemair <blacklemair@gmail.com>
2016-12-14 19:55:33 +0200

    slideshow

--- app/js/common.js
+++ app/js/common.js
@@ -58,3 +58,20 @@ $(function() {
 		$('header, section').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
 	});
 });
+
+{
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slideshow");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
