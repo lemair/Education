19a5ae05cd6a3175f2b9e9f1728cb7ca7b28092a
lemair <blacklemair@gmail.com>
2017-05-15 12:19:11 +0300

    mini-slider
    slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,23 @@
 $('.sl').slick({
-    dots: false,
-    prevArrow: false,
-    nextArrow: false
+	autoplay: true,
+  autoplaySpeed: 2000,
+  speed: 1000,
+	dots: false,
+	prevArrow: false,
+	nextArrow: false
+
+});
+$('.active-by-brand').slick({
+	speed: 700,
+	arrows: true,
+	dots: false,
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	prevArrow: '<button type="button" class="arrow-prev"><i class="zmdi zmdi-long-arrow-left"></i></button>',
+	nextArrow: '<button type="button" class="arrow-next"><i class="zmdi zmdi-long-arrow-right"></i></button>',
+	responsive: [
+	{ breakpoint: 991, settings: { slidesToShow: 3 }  },
+	{ breakpoint: 767, settings: { slidesToShow: 1 }  },
+	{ breakpoint: 479, settings: { slidesToShow: 1 }  }
+	]
 });
